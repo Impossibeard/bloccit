22f9de7064022349b295a84c1e0ad64f42b9749d
JD Crouch <jdcrouch5@gmail.com>
2015-11-12 22:10:54 +0900

    Add Unique Post & Comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,11 @@ end
 
 posts = Post.all
 
+uniqpost = Post.find_or_create_by(
+title: "I am a Uniq Title!",
+body: "I am a Uniq Body!"
+)
+
 #Create comments
 
 100.times do
@@ -18,6 +23,11 @@ posts = Post.all
   body: RandomData.random_paragraph
   )
 end
+
+uniqcomment = Comment.find_or_create_by(
+post: uniqpost,
+body: "I am a Uniq Comment Body!"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
