05a4821a8f1ddfbd5121064c80016cca6da42861
JD Crouch <jdcrouch5@gmail.com>
2015-11-12 11:48:41 +0900

    Update Question and Answer Specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) { Question.create!(title: "New Question Body" body: "New Question Body",) }
-  let(:answer) { Answer.create!(body: "Answer Body")}
+  let(:question) { Question.create!(title: "New Question Body", body: "New Question Body", resolved: false) }
+  let(:answer) { Answer.create!(body: "Answer Body", question: question)}
 
   describe "attributes" do
     it "should respond to body" do
       expect(answer).to respond_to(:body)
+    end
+    it "should respond to question" do
+      expect(answer).to respond_to(:question)
     end
   end
 end
